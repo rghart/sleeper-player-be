bec41b189e301759a28c996da6306d731f82467c
Ryan Hart <ryanghart91@gmail.com>
2023-08-17 15:23:31 -0500

    Fix player JSON

--- lib/sleeper_player_api_web/controllers/player_json.ex
+++ lib/sleeper_player_api_web/controllers/player_json.ex
@@ -28,19 +28,19 @@ defmodule SleeperPlayerApiWeb.PlayerJSON do
       player_json: player.player_json,
       active: player.active,
       age: player.age,
-      fantasy_positions: Enum.map(player.fantasy_positions, fn p -> p.abbreviation end),
+      fantasy_positions: Enum.map(Map.get(player, :fantasy_positions, []), fn p -> p.abbreviation end),
       first_name: player.first_name,
       last_name: player.last_name,
       full_name: player.full_name,
       player_id: player.player_id,
-      position: player.position.abbreviation,
+      position: (if player.position, do: player.position.abbreviation, else: nil),
       search_first_name: player.search_first_name,
       search_last_name: player.search_last_name,
       search_full_name: player.search_full_name,
       search_rank: player.search_rank,
-      status: player.status.status,
+      status: (if player.status, do: player.status.status, else: nil),
       years_exp: player.years_exp,
-      team: player.team.abbreviation
+      team: (if player.team, do: player.team.abbreviation, else: nil)
     }
   end
 end
